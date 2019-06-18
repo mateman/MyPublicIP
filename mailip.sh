46a60d121a93fdc4bf90d7247e991533ec1d4f6a
mateman <cuyeu@hotmail.com>
2019-06-18 13:36:26 -0300

    Update mailip.sh

--- mailip.sh
+++ mailip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo $(curl -s icanhazip.com )'; ' $(date) >>ip.txt
 sendmail \
--f unaventonpass@gmail.com \
--t pcuyeu@gmail.com \
+-f mailOrigen@gmail.com \
+-t mailDestino@gmail.com \
 -s smtp.gmail.com:587 \
 -o tls=yes \
 -u IP \
 -m $(tail -n 1 ip.txt | cut -d ";" -f 1) \
--xu unaventonpass \
--xp TwoPines1234
+-xu mailOrigen \
+-xp unaPassword
